409f8404d71d31ad263beaa9f52296853ea0260c
Uosnaridou <47666632+Uosnaridou@users.noreply.github.com>
2019-02-19 10:22:33 +0100

    Add files via upload

--- modele/modeleBillets.php
+++ modele/modeleBillets.php
@@ -22,27 +22,8 @@ class billets {
             return $reponse;
         }
     
-        public function commentaire(){
-            $bdd = $this->dbConnect();
-            $rep = $bdd->prepare('SELECT id, pseudo, commentaire, signalement FROM commentaires WHERE id_article = ?');
-            $rep->execute(array($_GET['articles']));
-            return $rep;
-        }
     
-        public function ajoutCommentaire(){
-            //On insere dans la base de données le pseudo poster et le commentaire dans le table commentaires
-            $bdd = $this->dbConnect();
-            if (isset($_POST['pseudo']) && isset($_POST['commentaire'])){
-            $req = $bdd->prepare('INSERT INTO commentaires(pseudo, commentaire, id_article) VALUES(:pseudo, :commentaire, :id_article)');
-            $req->execute(array(
-            'id_article' => htmlspecialchars($_GET['articles']),
-            'pseudo' => htmlspecialchars($_POST['pseudo']),
-            'commentaire' => htmlspecialchars($_POST['commentaire'])
-            ));
-            $req->closeCursor();
-            return $req; 
-		}    
-    }
+
     
     
     
